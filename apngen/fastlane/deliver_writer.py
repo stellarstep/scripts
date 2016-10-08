56aaff141bc0933098f395bec520487af34b8dfb
metasmile <cyrano905@gmail.com>
2016-10-08 13:09:47 +0900

    Merge commit 'a87f1a4c874bf5a7f8dbb36e17a59e50cef0ff3f' as 'apngen'

---
+++ apngen/fastlane/deliver_writer.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+'''
+[example : metadata.yml]
+Base:
+	support_url: http://support.url.com
+en-US:
+	metadata_dir_app1:
+			name: App Name
+   marketing_url: http://url.com
+ja:
+	metadata_dir_app1:
+			name: Japanese App Name
+   marketing_url: http://ja.url.com
+
+[result]
+fastlane/metadata_dir_app1/en-US/name.txt <- 'App Name'
+fastlane/metadata_dir_app1/en-US/marketing_url.txt <- 'http://url.com'
+fastlane/metadata_dir_app1/en-US/support_url.txt <- 'http://support.url.com'
+fastlane/metadata_dir_app1/ja/name.txt <- 'Japanese App Name'
+fastlane/metadata_dir_app1/ja/marketing_url.txt <- 'http://ja.url.com'
+fastlane/metadata_dir_app1/ja/support_url.txt <- 'http://support.url.com'
+'''
+
+import yaml, os, codecs
+
+#names
+__dirpath__=os.path.dirname(os.path.realpath(__file__))
+datas = yaml.safe_load(open(os.path.join(__dirpath__,'metadata.yml')))
+datas_base = datas['Base'] if 'Base' in datas else None
+
+def dict_merge(dct, merge_dct):
+	for k, v in merge_dct.iteritems():
+		if (k in dct and isinstance(dct[k], dict)
+				and isinstance(merge_dct[k], dict)):
+			dict_merge(dct[k], merge_dct[k])
+		else:
+			dct[k] = merge_dct[k]
+
+for dir, a, files in os.walk(__dirpath__):
+	l = os.path.basename(dir)
+	if l in datas:
+		data_lang = datas[l]
+		dict_merge(data_lang, datas_base)
+		for app, meta in [(k, data_lang[k]) for k in data_lang]:
+			for mfile in meta:
+				#base
+				p = os.path.join(__dirpath__, app, l, mfile+'.txt')
+				f = codecs.open(p, 'w', 'utf-8')
+				f.write(meta[mfile])
+				f.close()
+				print p,'\n',meta[mfile],'\n'
