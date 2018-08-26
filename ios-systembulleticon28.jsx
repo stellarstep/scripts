dd62814b8e09bd8cace1e438ce2bdab8ff56f2c2
metasmile <cyrano905@gmail.com>
2018-08-26 14:10:16 +0200

    Squashed 'ai2appiconset/' changes from 7e5eaac..a2b24e4
    a2b24e4 add ios-systembulleticon25.jsx
    bd6657c add system bullet icon
    29275b0 add table view icon.jsx
    79b36dd ios-marketing is not required "platform" property
    
    git-subtree-dir: ai2appiconset
    git-subtree-split: a2b24e49563f555c114168fbb460f6c32ba5fb8b

---
+++ ios-systembulleticon28.jsx
@@ -0,0 +1,6 @@
+PLATFORM = "ios"
+ICONS = [
+    "universal_28@2x",
+    "universal_28@3x"
+];
+#include "icon-exporter.jsx"
---
+++ ios-tableviewcellicon-exporter.jsx
@@ -0,0 +1,6 @@
+PLATFORM = "ios"
+ICONS = [
+    "universal_40@2x",
+    "universal_40@3x"
+];
+#include "icon-exporter.jsx"
